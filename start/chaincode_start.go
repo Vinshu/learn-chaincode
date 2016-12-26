2f3b6e9890c7b9cad93f0fbb8985686667b00482
Vinshu <guptavinshu@gmail.com>
2016-12-26 10:03:09 +0000

    Compiled my code

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -1,4 +1,5 @@
-/*
+/*Copyright IBM Corp 2016 All Rights Reserved.
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -155,5 +156,4 @@ func (t *SimpleChaincode) readPollData(stub shim.ChaincodeStubInterface, args []
 
     return valAsbytes, nil
 }
-
 
